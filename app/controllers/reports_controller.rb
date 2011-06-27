8f56932eeb5f4f345536cd12b729cf13f9ba383d
David Goodlad <david@goodlad.ca>
2011-06-27 15:09:14 +1000

    Skeleton implementation of reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,32 @@
 class ReportsController < ApplicationController
+  respond_to :html
+
+  def index
+    respond_with(@reports = Report.all)
+  end
+
+  def show
+    respond_with(@report = Report.find(params[:id]))
+  end
+
+  def new
+    respond_with(@report = Report.new)
+  end
+
+  def edit
+    respond_with(@report = Report.find(params[:id]))
+  end
+
+  def create
+    respond_with(@report = Report.create(params[:report]))
+  end
+
+  def update
+    respond_with(@report = Report.update(params[:id], params[:report]))
+  end
+
+  def destroy
+    @report = Report.destroy(params[:id])
+    respond_with(@report, :location => reports_url)
+  end
 end

b8738e73d7bd184557fe8e473d19373bad36e8f6
David Goodlad <david@goodlad.ca>
2011-06-27 13:36:22 +1000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmashReports3::Application.config.secret_token = '9fdb001f689dfd53cbcb47c1d51a18a1aaae391301723ccc02293db081703ecbfc676fe2983582702fdd125474601f1d2cdaea672b7da01e64e86b120fcb10f6'

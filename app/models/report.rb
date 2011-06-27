f28f2901b1e49c4c71385961451d91d02ae3b5dd
David Goodlad <david@goodlad.ca>
2011-06-27 20:27:43 +1000

    Reports can execute parameterised queries

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,29 @@
 class Report < ActiveRecord::Base
+  def db; DB end
+
+  def db; self.class.db end
+
+  def execute(params = {})
+    db.fetch sql_query_with_params(params)
+  end
+
+  def sql_query_with_params(params = {})
+    q = sql_query.dup
+    params.each { |k,v| q.gsub! field_regexp(k), "#{v}" }
+    q
+  end
+
+  def report_fields
+    return [] if sql_query.blank?
+    sql_query.scan(field_regexp).flatten.collect {|f| f.strip.to_sym }.uniq
+  end
+
+private
+  def field_regexp(for_field = nil)
+    if for_field
+      /<% *#{for_field} *%>/
+    else
+      /<%(.+?)%>/
+    end
+  end
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Report do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @db = double("Sequel::Mysql2::Database")
+    Report.stub(:db) { @db }
+  end
+
+  context "with a simple SQL query" do
+    before { subject.sql_query = "SELECT * FROM foos;" }
+
+    it "should execute the SQL query on the report db" do
+      @db.should_receive(:fetch).with("SELECT * FROM foos;")
+      subject.execute
+    end
+
+    it "should have an empty list of report fields" do
+      subject.report_fields.should == []
+    end
+  end
+
+  context "with a parameterised SQL query" do
+    before { subject.sql_query = "SELECT * FROM foos WHERE bar = '<% bar %>'"}
+
+    it "should execute the SQL query with the given parameter values" do
+      @db.should_receive(:fetch).with("SELECT * FROM foos WHERE bar = 'pants'")
+      subject.execute :bar => 'pants'
+    end
+
+    it "should have report fields" do
+      subject.report_fields.should == [:bar]
+    end
+  end
 end

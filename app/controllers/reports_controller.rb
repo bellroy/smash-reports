bed585992200252f6fa19e24a9a18858a5b905eb
David Goodlad <david@goodlad.ca>
2011-06-30 11:18:11 +1000

    Ensure all respond_with calls know about the nested resources
    This may not be absolutely necessary except in create/update/destroy, but better
    safe than sorry.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,35 +6,35 @@ class ReportsController < ApplicationController
   respond_to :csv, :only => :show
 
   def index
-    respond_with(@reports = @organization.reports.all)
+    respond_with(@organization, @reports = @organization.reports.all)
   end
 
   def show
     @report = @organization.reports.find_and_execute(params[:id], params[:field_values])
-    respond_with @report do |format|
+    respond_with @organization, @report do |format|
       format.csv { render :text => @report.to_csv }
     end
   end
 
   def new
-    respond_with(@report = @organization.reports.build)
+    respond_with(@organization, @report = @organization.reports.build)
   end
 
   def edit
-    respond_with(@report = @organization.reports.find(params[:id]))
+    respond_with(@organization, @report = @organization.reports.find(params[:id]))
   end
 
   def create
-    respond_with([@organization, @report = @organization.reports.create(params[:report])])
+    respond_with(@organization, @report = @organization.reports.create(params[:report]))
   end
 
   def update
-    respond_with([@organization, @report = @organization.reports.update(params[:id], params[:report])])
+    respond_with(@organization, @report = @organization.reports.update(params[:id], params[:report]))
   end
 
   def destroy
     @report = @organization.reports.destroy(params[:id])
-    respond_with(@report, :location => reports_url)
+    respond_with(@organization, @report, :location => reports_url)
   end
 
   private

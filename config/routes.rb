2cea563c00681770b48543529975ba6c7c064ef5
David Goodlad <david@goodlad.ca>
2011-07-04 13:14:29 +1000

    Shorten authentication URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SmashReports3::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:sessions] do
+    get "/login" => "devise/sessions#new", :as => :new_user_session
+    post "/login" => "devise/sessions#create", :as => :user_session
+    get "/logout" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
 
   resources :organizations, :path => '', :only => :index do
     resources :reports
